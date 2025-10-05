c111bac60f38d129ff4e462d81ddec4d867d6cdf
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2025-10-05 14:28:47 -0400

    feat: update layout and styles for homepage and main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import Main from "@/components/ui/structure/main";
 import Image from "next/image";
+import Link from "next/link";
+import Main from "@/components/ui/structure/main";
 
 export default function Home() {
   return (
     <Main>
-      <h1>Ponder Mars</h1>
+      <div className="flex-col md:flex-row flex justify-between items-start md:items-center gap-4">
+        <Link href="/" className="text-lg font-medium text-muted-foreground">
+          ponder mars
+        </Link>
+        <div className="flex items-center gap-4 text-base">
+          <Link href="/">music</Link>
+          <Link href="/">writing</Link>
+          <Link href="/">reading</Link>
+          <Link href="/">story</Link>
+        </div>
+      </div>
+      <div>
+        <p>
+        Software engineer, designer, and creative writer. Founder & CTO of Maxed.
+        </p>
+      </div>
     </Main>
   );
 }
--- src/components/ui/structure/main.tsx
+++ src/components/ui/structure/main.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 export default function Main({ children,className }: { children: React.ReactNode, className?: string }) {
 
   return (
-    <main className={cn("flex flex-col gap-6 mx-auto max-w-3xl my-12", className)}>
+    <main className={cn("flex flex-col gap-12 mx-auto max-w-2xl my-12 px-4 md:px-0", className)}>
       {children}
     </main>
   )
