52b9623bc558f0b7e2701043d0f356d4ea98482a
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2025-10-11 13:18:03 -0400

    chore: update layout in NotePage and Aside components

--- src/components/notes/aside.tsx
+++ src/components/notes/aside.tsx
@@ -9,7 +9,7 @@ export default function Aside({ children, translateY = 0 }: AsideProps) {
       className="absolute h-0 w-0 overflow-visible hidden lg:block"
       style={{ transform: `translateY(${translateY}px)` }}
     >
-      <div className="absolute w-[280px] left-[624px] aside">
+      <div className="absolute w-[280px] left-[700px] aside">
         <div className="h-[1px] bg-foreground opacity-20 mt-0.5 mb-2"/>
         <div className="prose-sm text-justify leading-snug">
           {children}
