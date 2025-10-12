40042931d1b283e252934aa2298145881ccd870f
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2025-10-12 17:45:24 -0400

    chore: update prep script and refine runtime environment configuration

--- src/env.ts
+++ src/env.ts
@@ -6,12 +6,17 @@ export const env = createEnv({
     RESEND_API_KEY: z.string(),
     RESEND_AUDIENCE_ID: z.string(),
   },
-  runtimeEnv: process.env,
+  runtimeEnv: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    RESEND_AUDIENCE_ID: process.env.RESEND_AUDIENCE_ID,
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  },
 
   clientPrefix: "NEXT_PUBLIC_",
   client: {
     NEXT_PUBLIC_POSTHOG_KEY: z.string(),
     NEXT_PUBLIC_POSTHOG_HOST: z.string(),
-  }
+  },
 
 });
