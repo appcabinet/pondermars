5d356f712a3b596ca8375d2b8146f8e7ba065f3a
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2025-10-28 21:38:28 -0400

    feat: add new audio tracks and update music component

--- src/components/design/music.tsx
+++ src/components/design/music.tsx
@@ -25,12 +25,20 @@ export default function Music() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <Audio
-          title="YEAH"
-          url="https://lhfsdw7dl5.ucarecd.net/b8b7f690-3c6e-4f27-a813-acd061523648/01Yeah.mp3"
+          title="Before I Fall"
+          url="https://lhfsdw7dl5.ucarecd.net/51824433-c312-4f18-ac6c-d4c5539de0f4/PonderMarsBeforeIFall16bit441khz.mp3"
         />
         <Audio
-          title="10X"
-          url="https://lhfsdw7dl5.ucarecd.net/f5a0aba7-90c6-45d5-9cdb-6780a9c00968/10x.mp3"
+          title="The Intergalactic Forest"
+          url="https://lhfsdw7dl5.ucarecd.net/3be008da-933a-4fd1-bd6e-3b0ebbe4a800/PonderMarsIntergalacticForestMaster.mp3"
+        />
+        <Audio
+          title="Bacterium"
+          url="https://lhfsdw7dl5.ucarecd.net/e8783021-6ef9-433e-b8d8-5628da093d0e/PonderMarsBacteriumFinalMaster441k16bit.mp3"
+        />
+        <Audio
+          title="Mandoguan"
+          url="https://lhfsdw7dl5.ucarecd.net/e9eb796c-4e20-4250-bb2b-f6597871426e/PonderMarsMoneyTroubles.mp3"
         />
       </div>
     </div>
